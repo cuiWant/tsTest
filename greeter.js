0494cf8ad3feef47c0253c8feac572e7490265a3
505521261@qq.com <cuiyh@tico.cn>
2020-04-08 17:05:32 +0800

    fix: '2020 4 8 commit exercise '

--- greeter.js
+++ greeter.js
@@ -1,22 +1,384 @@
 "use strict";
-// let someValue: string = 'this is String ';
-// let readStrLength = (<number>someValue.length)
-// let readStrLength2: string = someValue;
-// let readStrLength3: any = (someValue as string);
-// console.log(readStrLength, 'readStrLength')
-// console.log(readStrLength2, 'readStrLength2')
-// console.log(readStrLength3)
-var Person = /** @class */ (function () {
-    function Person(config) {
-        this.name = config.name;
-        this.sex = config.sex;
-        this.age = config.age;
+// // // // let someValue: string = 'this is String ';
+// // // // let readStrLength = (<number>someValue.length)
+// // // // let readStrLength2: string = someValue;
+// // // // let readStrLength3: any = (someValue as string);
+// // // // console.log(readStrLength, 'readStrLength')
+// // // // console.log(readStrLength2, 'readStrLength2')
+// // // // console.log(readStrLength3)
+// // // // 接口 interface  拦截器interceptor
+// // // // function  printLabel(data: { label: string }) {
+// // // // 	console.log(data.label)
+// // // // }
+// // // // let data = {
+// // // // 	label: '1',
+// // // // 	0: "1"
+// // // // };
+// // // // // data = { label: '1' };
+// // // // // data.s = 1
+// // // // printLabel(data)
+// // // // interface LabelledValue {
+// // // // 	label: string,
+// // // // 	0: number
+// // // // }
+// // // // function printLabel(data: LabelledValue) {
+// // // // 	console.log(data, 'data')
+// // // // }
+// // // // printLabel({ 0: 1, label: 'x' })
+// // // // interface SquareConfig {
+// // // // 	color?: string,
+// // // // 	width: number
+// // // // }
+// // // // function createSquareConfig(data: SquareConfig) {
+// // // // 	let square = {
+// // // // 		width: data.width * 4,
+// // // // 		background: ''
+// // // // 	}
+// // // // 	if (data.color) {
+// // // // 		square.background = data.color
+// // // // 	}
+// // // // 	return square
+// // // // }
+// // // // let config: SquareConfig = {
+// // // // 	color: "pink",
+// // // // 	width: 15,
+// // // // }
+// // // // console.log(
+// // // // 	createSquareConfig(config)
+// // // // )
+// // // // interface Config {
+// // // // 	color?: string,
+// // // // 	readonly width: number
+// // // // }
+// // // // function createSquareFn(data: Config): { area: number, color: string, edit: boolean } {
+// // // // 	let NewSquare = { edit: false, area: data.width * data.width, color: data.color || 'pink' }
+// // // // 	return NewSquare
+// // // // }
+// // // // let squareObj: Config = {
+// // // // 	width: 100
+// // // // };
+// // // // console.log(createSquareFn(squareObj))
+// // // // let a: number[] = [];
+// // // // // for (let i = 11; i > 0; i--) {
+// // // // // 	a.push(i)
+// // // // // 	let str: string = '* * * * * * * * * * *';
+// // // // // 	str = str.slice(0, i)
+// // // // // 	let flag: number = 10 - str.length
+// // // // // 	let nbsp = '';
+// // // // // 	for (let j = 11; j > i; j--) {
+// // // // // 		nbsp += " "
+// // // // // 		// j % 2 === 0 || j === 1 ? nbsp += " " : ''
+// // // // // 	}
+// // // // // 	let result = nbsp + str;
+// // // // // 	console.log(result)
+// // // // // }
+// // // // // a.push(1, 111)
+// // // // // let ro: ReadonlyArray<number> = a;
+// // // // // // ro[0] = 0; error
+// // // // // // ro.push(1); error
+// // // // // // a.push(2) success
+// // // // // // a = ro; error
+// // // // // a.push(22)
+// // // // // a = ro as number[]
+// // // // // console.log(ro);
+// // // // // console.log(a);
+// // // // interface SquareConfig {
+// // // // 	color?: string,
+// // // // 	width?: number,
+// // // // }
+// // // // function createSquareConfig(config: SquareConfig): { area: number, color: string } {
+// // // // 	let NewSquare = {
+// // // // 		area: config.width ? config.width * config.width : 0,
+// // // // 		color: config.color ? config.color : ''
+// // // // 	};
+// // // // 	return NewSquare
+// // // // }
+// // // // const config = {
+// // // // 	color: 'pink',
+// // // // 	name: 'cui',
+// // // // 	// width: 15,
+// // // // }
+// // // // console.log(createSquareConfig(<SquareConfig>{
+// // // // 	color: 'pink',
+// // // // 	name: 'cui',
+// // // // 	// width: 15,
+// // // // }))
+// // // // // console.log(document, 'document')4
+// // // // interface SearchFunc {
+// // // // 	(source: string, sunString: string): boolean
+// // // // }
+// // // // let mySearch: SearchFunc;
+// // // // mySearch = function (source: string, subString: string): boolean {
+// // // // 	let result = source.search(subString);
+// // // // 	console.log(source, subString, result)
+// // // // 	return result > 1
+// // // // }
+// // // // console.log(mySearch('00xxx', 'x'))
+// // // // function 
+// // // // interface StringArray {
+// // // // 	[index: number]: object
+// // // // }
+// // // // let myArray: StringArray;
+// // // // myArray = [[], {}];
+// // // // let myStr: StringArray = myArray[0];
+// // // // class Animal {
+// // // // 	name: string;
+// // // // 	constructor(name: string) {
+// // // // 		this.name = name
+// // // // 	}
+// // // // }
+// // // // class Dog extends Animal {
+// // // // 	breed: string;
+// // // // 	constructor(props: object) {
+// // // // 		let { name: string, sex: string = '' } = props;
+// // // // 		super(name)
+// // // // 		this.breed = sex;
+// // // // 	}
+// // // // 	say() {
+// // // // 		console.log(this.name + this.breed)
+// // // // 	}
+// // // // }
+// // // // console.log(new Dog({ name: '奔奔', sex: 'boy' }))
+// // // // interface NotOkay {
+// // // // 	[x: number]: Animal;
+// // // // 	[x: string]: Dog;
+// // // // }
+// // // // interface NumberDictionary{
+// // // // 	[index:string] :number;
+// // // // 	length:number;
+// // // // }
+// // // // class Greeter {
+// // // // 	name: string;
+// // // // 	constructor(name: string) {
+// // // // 		this.name = name
+// // // // 	}
+// // // // 	say() {
+// // // // 		console.log(this.name + '先生')
+// // // // 	}
+// // // // }
+// // // // let greeter = new Greeter('崔');
+// // // // greeter.say()
+// // // // class Animal {
+// // // // 	move(distanceInMeters: number = 0, animal: string) {
+// // // // 		console.log('this is ' + animal + ' run ' + distanceInMeters + '!!')
+// // // // 	}
+// // // // }
+// // // // class Dog extends Animal {
+// // // // 	bark() {
+// // // // 		console.log('woof , woof ')
+// // // // 	}
+// // // // }
+// // // // let dog = new Dog()
+// // // // dog.bark();
+// // // // console.dir(dog.constructor);
+// // // // dog.move(10, );
+// // // // class Animal {
+// // // // 	public name: string;
+// // // // 	public constructor(theName: string) { this.name = theName };
+// // // // 	public move(distanceInMeters: number) {
+// // // // 		console.log(distanceInMeters)
+// // // // 	}
+// // // // }
+// // // // let person = new Animal('cuiwant');
+// // // // console.log(person.name)
+// // // // class Animal {
+// // // // 	private name: string;
+// // // // 	constructor(name: string) {
+// // // // 		this.name = name
+// // // // 		console.log(name, 'xx')
+// // // // 	}
+// // // // 	say() {
+// // // // 		// console.log(name)  ''
+// // // // 		// console.log(this.name)
+// // // // 		return this.name
+// // // // 	}
+// // // // }
+// // // // let person = new Animal('cui');
+// // // // console.log(person.say(), 'name')
+// // // // console.log(person.name, 'person')
+// // // // console.dir(Animal, 'ss')
+// // // // class Animal {
+// // // // 	protected name: string;
+// // // // 	constructor(name: string) {
+// // // // 		this.name = name
+// // // // 	}
+// // // // }
+// // // // class Dog extends Animal {
+// // // // 	constructor(name: string) {
+// // // // 		super(name)
+// // // // 	}
+// // // // }
+// // // // let animal = new Animal('cui');
+// // // // let dog = new Dog('wangcai');
+// // // // let betray = {};
+// // // // 背叛 betray
+// // // // betray.__proto__ = dog.__proto__;
+// // // // console.log(betray)
+// // // // animal = dog;
+// // // // animal = {}; //error
+// // // // interface personConfig {
+// // // // 	readonly name?: string;
+// // // // 	readonly sex?: boolean;
+// // // // 	readonly age?: number;
+// // // // }
+// // // // class Person {
+// // // // 	readonly config: any;
+// // // // 	readonly name?: string;
+// // // // 	readonly sex?: boolean;
+// // // // 	readonly age?: number;
+// // // // 	constructor(config: personConfig) {
+// // // // 		this.name = config.name;
+// // // // 		this.sex = config.sex;
+// // // // 		this.age = config.age;
+// // // // 	}
+// // // // }
+// // // // let person: Person = new Person({
+// // // // 	name: 'cui',
+// // // // 	age: 123,
+// // // // 	sex: true
+// // // // })
+// // // // console.log(person, 'person')
+// // // // class Employee {
+// // // // 	fullName: string;
+// // // // }
+// // // // let employee: Employee = new Employee()
+// // // // employee.fullName = 'cui';
+// // // // if (employee.fullName) {
+// // // // 	console.log(employee.fullName);
+// // // // }
+// // // let passcoede = "secrest passcode";
+// // // class Employee {
+// // // 	private _fullName: string;
+// // // 	get fullName(): string {
+// // // 		return this._fullName
+// // // 	}
+// // // 	set fullName(name: string) {
+// // // 		if (passcoede && passcoede === 'secrest passcode') {
+// // // 			this._fullName = name;
+// // // 		} else {
+// // // 			alert('passcode error')
+// // // 		}
+// // // 	}
+// // // }
+// // // let employee: Employee = new Employee();
+// // // employee.fullName = ' cui yong hao ';
+// // // if (employee.fullName) {
+// // // 	alert(employee.fullName)
+// // // }
+// // // let anyThing: any = '1';
+// // // anyThing();
+// // // console.log(anyThing)
+// // // let str = 1;
+// // // str = '1';
+// // // let str: string | number | boolean | undefined | null
+// // // interface Person {
+// // // 	name: string,
+// // // 	age: number,
+// // // 	readonly id: string | number
+// // // }
+// // // let user: Person = {
+// // // 	name: 'ss',
+// // // 	age: Infinity,
+// // // 	id: 1
+// // // }
+// // // user.id = user.name;
+// // // str = {}
+// // // let arr: number[] = [1, 2, 3, 4, 5, 6];
+// // // arr.push('1') //error
+// // // function createArray(length: number, value: any): Array<any> {
+// // // 	let result = [];
+// // // 	for (let i = 0; i < length; i++) {
+// // // 		result[i] = value
+// // // 	}
+// // // 	return result
+// // // }
+// // // console.log(createArray(2, false))
+// // // function swap<U, T, F>(tuple: [T, U, T, F]): [U, T] {
+// // // 	console.log()
+// // // 	return [tuple[1], tuple[0]]
+// // // }
+// // // swap([1, 8, 1, 2])
+// // interface Person {
+// // 	name: string,
+// // 	age?: number,
+// // 	readonly id: any
+// // }
+// // // let user: Person = {
+// // // 	name: 'cuiyonghao',
+// // // 	age: 25,
+// // // 	id: 199602040615
+// // // }
+// // let value = '1';
+// // // delete user.age
+// // // delete user.name 编译不出错
+// // // function test(data: any): Array<number> {
+// // // 	let arr: number[] = [];
+// // // 	// arr[0] = 2;
+// // // 	arr.push(data)
+// // // 	arr.push(data)
+// // // 	return arr
+// // // }
+// // // console.log(test(value))
+// // function sum() {
+// // 	// arguments.index = 1;
+// // 	let args: IArguments = arguments
+// // }
+// // let arr: string[] = ['', '']
+// // console.log(arr, 'arr')
+// // arr.push(1);  //error
+// // let arr: object[] = [];
+// // arr.push({}, [], function () { });
+// // arr[5] = 1;
+// // interface Point {
+// // 	readonly console: string;
+// // 	log?: any;
+// // }
+// // let fn = <Point>(data: Point): Point => {
+// // 	return data
+// // }
+// // let param: Point = {
+// // 	console: 'cui',
+// // 	log: 2
+// // }
+// // console.log(fn(param))
+// // let arr = ['f', 'i', 'g', 'h', 't', 'i', 'n', 'g']
+// // for (let ar in arr) {
+// // 	console.log(ar)
+// // }
+// var fn = function (value: boolean): boolean {
+// 	// console.log(, 'this')
+// 	console.log(globalThis);
+// 	// this
+// 	// console.log(, 'this');
+// 	return value
+// }
+// fn(true)
+// function identity<T>(arg: T): T {
+// 	return arg
+// }
+// let output = identity<number>(1)
+// function identity<T>(arg: T[]): T[] {
+// 	console.log(arg.length)
+// 	return arg
+// }
+// function identity<T>(arg: Array<T>): Array<T> {
+// 	return arg
+// }
+// function identity<t>(arg: t): t {
+// 	return arg
+// }
+// let _identity: <T>(arg: T) => T = identity;
+// 拦截器interceptor
+// interface Identity<T> {
+// 	(arg: T): string
+// }
+// function identity<T>(arg: T): string {
+// 	return '1'
+// }
+// let _identity: Identity<number> = identity;
+var GenericNumber = /** @class */ (function () {
+    function GenericNumber(arg) {
+        this.name = arg;
     }
-    return Person;
+    return GenericNumber;
 }());
-var person = new Person({
-    name: 'cui',
-    age: 123,
-    sex: true
-});
-console.log(person, 'person');
--- typeScript.md
+++ typeScript.md
@@ -1,8 +1,12 @@
 ###类型 
 	string number boolean null undefined object function array any(任意类型) never(到达不了重点即出错死循环。) void (空值 function 返回为undefind or null)
+	变量声明的时候未指定其类型 为any
+	类型推论 声明时赋值一个类型 ts 回推断他是什么类型如果 后面更改推断的类型 就会出现error
+	| ：或者类型  let str : string | boolean  如果 boolean  被使用length 属性编译报错 ts发现boolean  没有length
+	数组 +[] 表示
 	(1)	类型断言 两种方式 （确定类型不需要ts 检查）1. (<string>someValue) 2. as 语法(someValue as string ) as 语法适用于尖括号被特别解析 如:jsx
 	(2)  定义变量与js 相同 及作用域 var let const  
-	(3)	接口 命名大驼峰命名 第三方代码定义契约 地址传入 规定值类型 function (lab : {label : string }){}  interface 接口 定义公用 接口 interface  labelValue {}
+	(3)对象的类型 =>	接口 命名大驼峰命名 第三方代码定义契约 地址传入 规定值类型 function (lab : {label : string }){}  interface 接口 定义公用 接口 interface  labelValue {}
 	interface 属性名加? 可选属性  只读属性 readonly x: number 函数参数后面跟 格式 是返回值的属性
 	(4)  只读数组 关键字ReadonlyArray  readonly vs const  前者是属性 后者是变量
 	(5) 额外的属性 在给函数传参的时候 当我们使用字面量传参有额外属性时 会发生错误 怎么绕过错误那 意识断言 <xx>  as xx  或者interface { [propName:string] :any}
@@ -16,3 +20,20 @@
 		protected 受保护的 实例 子类可以访问  其他不允许访问
 	(9)	readonly 修饰符
 		使用readonly 关键字将属性设置为只读。 只读属性必须在声明时或构造函数里被初始化。
+	(10) 参数属性
+		我们可以在参数传入的时候保护他 (private,protected)
+		有一个属性只是在 类中可见 但是传入进来我们又要立马给 他赋值 既可以 使用这种方式；
+	(11) 存取器
+	 		类中 加set  get  关键字 的函数 会被储存为 读取更改
+	(12) 泛型 generics 
+		在定义函数。接口或者 类的时候 不预先制定具体类型而在使用的时候在指定类型的一种特性。
+		多个类型的参数 
+			定义泛型的时候，可以一起定义多个类型参数
+			在创建函数的时候 定义一个 自定义类型的 类型 使用的时候 声明需求类型  eg ： let output  = identity<string> (2) //error this is not string 
+			泛型类型 有一个类型参数在上面 就像函数生命一样
+			由此可以推理到用接口 ( interface );
+			泛型类  到这里了。。。
+
+	(13)类数组 (Array-like Object) 不是数组类型 arguments dom标签
+	(14) 内置对象
+		
